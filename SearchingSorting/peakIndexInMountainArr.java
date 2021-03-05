1e9537522465db21aaebdd06211a89d78c0d6fa4
SridharPrasad-Panneerselvamçclear <panneerselvam.s@northeastern.edu>
2021-03-05 17:33:25 -0500

    sortingProblems-part2

---
+++ SearchingSorting/peakIndexInMountainArr.java
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/
+
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+	for (int i = 1; i < arr.length - 1; i++) {
+		if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) return i;
+	}
+	return -1;
+}
+}
